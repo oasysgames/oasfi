a81d78a962bbc36e17a26e25bd96a9f39f708182
girafferz <noiman2004+git@gmail.com>
2023-10-19 18:06:28 +0900

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
-    project: ["tsconfig.json"],
+    sourceType: 'module',
+    project: ['tsconfig.json'],
   },
   rules: {
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      { endOfLine: 'auto', singleQuote: true, trailingComma: 'all' },
+    ],
   },
-  ignorePatterns: ["**/*.types.ts", "**/*.js"],
+  ignorePatterns: ['**/*.types.ts', '**/*.js'],
 };
